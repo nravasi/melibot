26c06f807fc26bae85b07ec3097aa4a052bfe8c2
Nicolas Martin Ravasi <nicolas.ravasi@mercadolibre.com>
2016-05-05 21:33:53 -0300

    fix weburl

--- item_formatter.js
+++ item_formatter.js
@@ -3,13 +3,13 @@ var util = require('util');
 
 var formatItems = function(results){
 	return _.map(results, function(it){
-		console.log(util.inspect(it))
+		//console.log(util.inspect(it))
 		return {
 			title: it.title,
 			subtitle: '$' + it.price,
 			image_url: it.thumbnail,
 			buttons: [{
-				type: web_url,
+				type: 'web_url',
 				url: it.permalink,
 				title: "Ver en MercadoLibre"
 			}]
