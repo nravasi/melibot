50e3c45b0a4ea2b00eea2ffddff57d7936b47641
aaronson <alexaronson@gmail.com>
2016-05-08 20:58:46 -0300

    Prueba de varios campos

--- item_formatter.js
+++ item_formatter.js
@@ -6,7 +6,7 @@ var formatItems = function(results){
 		//console.log(util.inspect(it))
 		return {
 			title: it.title,
-			subtitle: '$' + it.price,
+			subtitle: '$' + it.price + '\b testeo \b testeo2',
 			image_url: _.replace(it.thumbnail, '-I', '-O'),	
 			buttons: [{
 				type: 'web_url',
