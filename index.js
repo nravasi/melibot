a00009cf8b12b9e002fdb995920bfe3a17adea68
Nicolas Martin Ravasi <nicolas.ravasi@mercadolibre.com>
2016-05-13 10:22:14 -0300

    fallback

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(bodyParser.urlencoded({
     extended: false
 }))
 
+var useWit = false;
+
 // Process application/json
 app.use(bodyParser.json())
 
@@ -43,29 +45,34 @@ app.post('/webhook/', function(req, res) {
 
         if (event.message && event.message.text) {
             var sessionId = findOrCreateSession(event.sender.id);
-            wit.runActions(
-                sessionId,
-                event.message.text,
-                sessions[sessionId].context,
-                (error, context) => {
-                    if (error) {
-                        console.log('Oops! Got an error from Wit:', error);
-                    } else {
-                        // Our bot did everything it has to do.
-                        // Now it's waiting for further messages to proceed.
-                        console.log('Waiting for futher messages.');
 
-                        // Based on the session state, you might want to reset the session.
-                        // This depends heavily on the business logic of your bot.
-                        // Example:
-                        // if (context['done']) {
-                        //   delete sessions[sessionId];
-                        // }
+            if (useWit) {
+                wit.runActions(
+                    sessionId,
+                    event.message.text,
+                    sessions[sessionId].context,
+                    (error, context) => {
+                        if (error) {
+                            console.log('Oops! Got an error from Wit:', error);
+                        } else {
+                            // Our bot did everything it has to do.
+                            // Now it's waiting for further messages to proceed.
+                            console.log('Waiting for futher messages.');
 
-                        // Updating the user's current session state
-                        sessions[sessionId].context = context;
-                    }
-                })
+                            // Based on the session state, you might want to reset the session.
+                            // This depends heavily on the business logic of your bot.
+                            // Example:
+                            // if (context['done']) {
+                            //   delete sessions[sessionId];
+                            // }
+
+                            // Updating the user's current session state
+                            sessions[sessionId].context = context;
+                        }
+                    });
+            } else {
+                searchClient.search(event.sender.id, event.message.text, handleClientResponse);
+            }
         }
     }
     res.sendStatus(200);
