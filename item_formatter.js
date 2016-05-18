97dea4bcc98eb2b65b74888470c884b8abe7efb8
aaronson <alexaronson@gmail.com>
2016-05-17 23:40:00 -0300

    Prueba de varios campos

--- item_formatter.js
+++ item_formatter.js
@@ -5,10 +5,10 @@ var formatItems = function(results){
 
 	return _.map(results, function(it){
 		//console.log(util.inspect(it))
-
+		var subtitlePrefix = it.installments.quantity+'x $'+it.installments.amount
 		return {
 			title: '$ ' + it.price,
-			subtitle: subtitlePrefix + '\n' +it.title,
+			subtitle: subtitlePrefix + '\n' + it.title,
 			image_url: _.replace(it.thumbnail, '-I', '-O'),	
 			item_url: it.permalink
 			/*buttons: [{
