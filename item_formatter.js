2a5eaf5a78eb98edcd8b0e813872588275450487
aaronson <alexaronson@gmail.com>
2016-05-17 23:20:45 -0300

    Prueba de varios campos

--- item_formatter.js
+++ item_formatter.js
@@ -2,11 +2,16 @@ var _ = require("lodash");
 var util = require('util');
 
 var formatItems = function(results){
+
 	return _.map(results, function(it){
 		//console.log(util.inspect(it))
+		var subtitlePrefix = it.installments.quantity+'x $'+it.installments.amount
+		for (int i=subtitlePrefix.length; i<31;i++){
+			subtitlePrefix = subtitlePrefix + ' ' 
+		}
 		return {
-			title: '$' + it.price,
-			subtitle: it.title,
+			title: '$ ' + it.price,
+			subtitle: subtitlePrefix + it.title,
 			image_url: _.replace(it.thumbnail, '-I', '-O'),	
 			item_url: it.permalink
 			/*buttons: [{
