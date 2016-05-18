7fb848f91d936684f290ac1c9d1de3e3c20b770a
aaronson <alexaronson@gmail.com>
2016-05-17 21:07:36 -0300

    Prueba de varios campos

--- item_formatter.js
+++ item_formatter.js
@@ -5,14 +5,15 @@ var formatItems = function(results){
 	return _.map(results, function(it){
 		//console.log(util.inspect(it))
 		return {
-			title: it.title,
-			subtitle: '$' + it.price,
+			title: '$' + it.price,,
+			subtitle: it.title,
 			image_url: _.replace(it.thumbnail, '-I', '-O'),	
-			buttons: [{
+			item_url: it.permalink
+			/*buttons: [{
 				type: 'web_url',
 				url: it.permalink,
 				title: "Ver en MercadoLibre"
-			}]
+			}]*/
 		}
 	})
 }
